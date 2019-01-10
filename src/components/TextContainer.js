323891fc91c8088c4de0150509f0bf8625d58457
Jonathan Lattanzi <jonathanlattanzi@gmail.com>
2019-01-10 16:18:08 -0600

    Project finished

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -1,35 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 export default class TextContainer extends Component {
-  constructor() {
-    super()
-    this.state = {
-      text: ''
+    constructor() {
+        super()
+        this.state = {
+            text: '',
+        }
+
+        this.updateText = this.updateText.bind(this)
     }
-  }
 
-  updateText(e) {
-    this.setState({
-      text: e.target.value
-    })
-  }
+    updateText(e) {
+        this.setState({
+            text: e.target.value,
+        })
+    }
 
-  render() {
-    return (
-      <div className="textContainer">
-        <textarea 
-          // style={  }
-          onChange={this.updateText}
-          value={this.state.text} 
-          placeholder='Start typing your thoughts here!'
-          cols="90"
-          rows="30">
-        </textarea>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="textContainer">
+                <textarea
+                    style={{
+                        color: this.props.fontColor,
+                        fontFamily: this.props.fontFamily,
+                        fontSize: this.props.fontSize,
+                        borderStyle: 'none',
+                    }}
+                    onChange={this.updateText}
+                    value={this.state.text}
+                    placeholder="Start typing your thoughts here!"
+                    cols="90"
+                    rows="30"
+                />
+            </div>
+        )
+    }
 }
-
-
-
-
